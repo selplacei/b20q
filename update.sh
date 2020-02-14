30a38a4b749668f1a9b36e5face2674d9b7872d2
selplacei <35859522+selplacei@users.noreply.github.com>
2020-02-14 10:42:51 -0800

    Change update script

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # Script to update b20q. Edit this if your configuration is something other than
 # simply a git repo cloned from upstream.
+git fetch --all
+git reset --hard origin/master
 git pull
+
