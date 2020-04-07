4643b14dd94598e13d8f561c49d63f61c03074e6
selplacei <35859522+selplacei@users.noreply.github.com>
2020-04-06 19:00:10 -0700

    Add message length limit and split messages if it is exceeded.

--- commands.py
+++ commands.py
@@ -85,9 +85,11 @@ async def on_mod_only_fail(message):
 
 async def start(message):
 	if game.active:
-		await game.channel.send(	f'A game is already running! The current defender is {game.defender}.\n'
-							f'Hint: the current defender can use `{game.prefix} end` to end the game prematurely.\n'
-							f'Alternatively, a moderator can use `{game.prefix} force end` to end the current game.')
+		await game.channel.send(
+			f'A game is already running! The current defender is {game.defender}.\n'
+			f'Hint: the current defender can use `{game.prefix} end` to end the game prematurely.\n'
+			f'Alternatively, a moderator can use `{game.prefix} force end` to end the current game.'
+		)
 		return
 	if message.mentions and message.content.startswith(f'{game.prefix} start <@'):
 		if message.author == game.winner:
@@ -96,8 +98,10 @@ async def start(message):
 			await game.await_confirmation([user], message.channel, lambda u: game.start(user))
 			return
 		else:
-			await game.channel.send(f'{message.author.mention} Only the previous game\'s winner can request someone '
-									f'else to be the defender.')
+			await game.channel.send(
+				f'{message.author.mention} Only the previous game\'s winner can request someone '
+				f'else to be the defender.'
+			)
 			return
 	if (not game.winner) or message.author == game.winner:
 		# The game is not active, and the caller isn't interfering with the previous winner's priority.
@@ -109,15 +113,17 @@ async def start(message):
 
 
 async def show(message):
-	await game.channel.send(status_format.apply(
-			game.defender,
-			game.status['answers'],
-			game.max_questions,
-			game.status['hints'],
-			game.status['guesses'],
-			game.status['guess_queue'].items(),
-			game.max_guesses
-	))
+	await status_format.send(
+		game.channel,
+		2000,
+		game.defender,
+		game.status['answers'],
+		game.max_questions,
+		game.status['hints'],
+		game.status['guesses'],
+		game.status['guess_queue'].items(),
+		game.max_guesses
+	)
 
 
 async def help_(message):
--- status_format.py
+++ status_format.py
@@ -2,6 +2,8 @@
 from typing import List, Tuple
 from discord import User
 
+BREAK_POINT = '\ue000\ue000\ue000'
+
 
 def _get_name(user):
 	try:
@@ -20,6 +22,8 @@ def apply(
 		max_guesses: int  # -1 for unlimited
 	):  # so sad
 	# Construct the formatted string here and then return it.
+	# Wherever the `BREAK_POINT` substring (defined at the start of this file) appears,
+	# the message may be broken up into multiple parts if its length exceeds Discord's limit.
 	formatted = ''
 
 	# Defender
@@ -28,14 +32,14 @@ def apply(
 	formatted += (f'"{_get_name(defender)} ({defender})"' if defender else 'None (the game is currently not active)')
 
 	# Answers
-	formatted += '``` ```diff'
+	formatted += f'``` {BREAK_POINT}```diff'
 	if not answers:
 		formatted += '\nNo answers so far.'
 	for i, (correct, answer) in enumerate(answers):
 		formatted += f'\n{"+" if correct else "-"} [{i + 1}] {answer}'
 
 	# Questions/guesses left
-	formatted += '``` ```py\n'
+	formatted += f'``` {BREAK_POINT}```py\n'
 	if max_questions == -1:
 		formatted += 'You have unlimited questions.\n'
 	else:
@@ -46,14 +50,14 @@ def apply(
 		formatted += f'You have {(max_guesses - len(guesses))} guesses left.'
 
 	# Hints
-	formatted += '``` ```bat\n'
+	formatted += f'``` {BREAK_POINT}```bat\n'
 	formatted += f'Hints: {"None" if not hints else ""}'
 	for i, hint in enumerate(hints):
 		formatted += f'\n[{i + 1}] {hint}'
 
 	# Guesses
 	if guesses or guess_queue:
-		formatted += '``` ```diff\n'
+		formatted += f'``` {BREAK_POINT}```diff\n'
 		formatted += 'Guesses:\n'
 		for i, (correct, guesser, guess) in enumerate(guesses):
 			formatted += f'{"+" if correct else "-"} [{i + 1}] {_get_name(guesser)}: {guess}\n'
@@ -64,3 +68,24 @@ def apply(
 		formatted += '``` ```\nNo guesses so far.```'
 
 	return formatted
+
+
+async def send(channel, max_length, defender, answers, max_questions, hints, guesses, guess_queue, max_guesses):
+	fragments = apply(
+		defender, answers, max_questions, hints, guesses, guess_queue, max_guesses
+	).split(BREAK_POINT)
+	parts = [fragments.pop(0)]
+	while fragments:
+		if len(parts[-1] + fragments[0]) <= max_length:
+			parts[-1] += fragments.pop(0)
+		elif len(fragments[0]) > max_length:
+			whole_fragment = fragments.pop(0)
+			while len(whole_fragment) > max_length:
+				parts.append(whole_fragment[:max_length])
+				parts.append('**[Message split due to exceeding maximum length. Formatting may be broken.]**')
+				whole_fragment = whole_fragment[max_length:]
+			parts.append(whole_fragment)
+		else:
+			parts.append(fragments.pop(0))
+	for part in parts:
+		await channel.send(part)
